b065003b290849f44a32be36b72bda67a46fd3a5
P-Odiix <mahingarodin@gmail.com>
2025-01-08 08:34:31 +0200

    "added the insertionsort algorithm"

---
+++ Sorting/InsertionSort.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+void insertionSort(int arr[], int size) {
+    for (int i = 1; i < size; i++) {
+        int temp = arr[i];
+        int j = i - 1;
+        while (j>=0 && arr[j] > arr[temp]) {
+            arr[j-1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = arr[temp];
+    }
+}
+
+int main() {
+    int arr[] = {5, 3, 8, 4, 2};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Unsorted array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    insertionSort(arr, size);
+    cout << "Sorted array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
