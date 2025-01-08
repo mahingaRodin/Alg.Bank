ff73ca0ac82847f1e79ccac635e0d3133a3b08e2
P-Odiix <mahingarodin@gmail.com>
2025-01-08 08:40:29 +0200

    "debugged some errors in it"

--- Sorting/InsertionSort.cpp
+++ Sorting/InsertionSort.cpp
@@ -5,11 +5,11 @@ void insertionSort(int arr[], int size) {
     for (int i = 1; i < size; i++) {
         int temp = arr[i];
         int j = i - 1;
-        while (j>=0 && arr[j] > arr[temp]) {
-            arr[j-1] = arr[j];
+        while (j>=0 && arr[j] > temp) {
+            arr[j+1] = arr[j];
             j--;
         }
-        arr[j + 1] = arr[temp];
+        arr[j + 1] = temp;
     }
 }
 
@@ -24,6 +24,7 @@ int main() {
     cout << endl;
 
     insertionSort(arr, size);
+
     cout << "Sorted array: ";
     for (int i = 0; i < size; i++) {
         cout << arr[i] << " ";
