a08f308f45813965b6771a253915160cf6459c16
P-Odiix <mahingarodin@gmail.com>
2025-01-08 07:58:03 +0200

    "implemented the first sorting algorithm(Bubble Sort)"

---
+++ Sorting/BubbleSort.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+// Bubble Sort Algorithm
+void bubbleSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap the elements
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int arr[] = {5, 1, 4, 2, 8};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Original array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    bubbleSort(arr, size);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
+//challenge : use the vector library and swap method to make the code look short .
