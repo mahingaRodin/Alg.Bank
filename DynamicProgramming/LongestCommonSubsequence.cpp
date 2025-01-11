20ed42fcd65c998db38fcc6851105c67d670f9ba
P-Odiix <mahingarodin@gmail.com>
2025-01-11 18:50:30 +0200

    "implemented the lcs dynamic programming "

---
+++ DynamicProgramming/LongestCommonSubsequence.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+//here is the implementation of the lcs dynamic programming algorithm 
+
+int lcs(string str1, string str2) {
+    int n = str1.size();
+    int m = str2.size();
+
+    //create the dynamic programming table
+    int dp[100][100] = {0}; // assuming the max size of the string is < 100
+
+    //fill the table
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= m; j++) {
+            if(str1[i-1] == str2[j-1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1; // character matches
+            } else {
+                //take the best option
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    return dp[n][m]; // the length of the longest common subsequence
+}
+
+
+int main() {
+    string str1, str2;
+
+    cout << "Enter the first string: ";
+    cin >> str1;
+    cout << endl;
+
+    cout << "Enter the second string: ";
+    cin >> str2;
+    cout << endl;
+
+    cout << "the lcs of the two strings is " << lcs(str1, str2) << endl;
+    return 0;
+}
