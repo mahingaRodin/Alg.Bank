ed32c47193d1c212aeec32d5c62a0d4a34a558be
P-Odiix <mahingarodin@gmail.com>
2025-01-13 06:14:06 +0200

    "updated the codes "

--- LinkedLists/SinglyLinkedFns.cpp
+++ LinkedLists/SinglyLinkedFns.cpp
@@ -8,9 +8,28 @@ class Node {
 };
 
 // method to display a linked list
-int display(Node *head) {
+void display(Node *head) {
     while(head != NULL) {
-        cout << head->data;
+        cout << head->data<<" ";
         head = head->next;
     }
+    cout << "NULL" << endl;
+}
+
+
+int main() {
+    Node* n1 = new Node();
+    Node* n2 = new Node();
+    Node* n3 = new Node();
+
+    n1->data=10;
+    n1->next=n2;
+    n2->data = 20;
+    n2->next=n3;
+    n3->data = 30;
+    n3->next = NULL;
+
+    display(n1);
+    return 0;
+
 }
