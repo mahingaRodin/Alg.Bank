8fda7c67f69d796ba8d917b9fbefec66a8459c87
P-Odiix <mahingarodin@gmail.com>
2025-01-14 07:08:49 +0200

    "updated the codes and debugged some of the errors in it"

--- LinkedLists/implementationofAll.cpp
+++ LinkedLists/implementationofAll.cpp
@@ -13,37 +13,35 @@ struct Node {
 Node *head = NULL;
 
 void display(Node* head) {
-    while(head != NULL) {
-        cout<<head->data<<" ";
+    while (head != NULL) {
+        cout << head->data << " ";
         head = head->next;
     }
     cout << "NULL" << endl;
 }
 
 void addAtHead(Node **head, int val) {
-    Node *n = new Node(val);
-    n->data = val;
-    n->next = *head;
-    *head = n;
+    Node *n = new Node(val); // Create a new node
+    n->next = *head;         // Point the new node to the current head
+    *head = n;               // Update head to the new node
 }
 
-
 Node* addAtTail(Node* head, int val) {
-    Node *n = new Node(val);
-    if(head == NULL) {
-        return n;
+    Node *n = new Node(val); // Create a new node
+    if (head == NULL) {
+        return n; // If the list is empty, return the new node as the head
     }
     Node *tail = head;
-    while(tail->next != NULL) {
+    while (tail->next != NULL) { // Traverse to the end of the list
         tail = tail->next;
     }
-    tail->next = n;
+    tail->next = n; // Attach the new node at the tail
     return head;
 }
 
 int length(Node* head) {
     int count = 0;
-    while(head != NULL) {
+    while (head != NULL) {
         count++;
         head = head->next;
     }
@@ -51,29 +49,29 @@ int length(Node* head) {
 }
 
 int main() {
-    Node *n1 = NULL;
-    Node *n2 = NULL;
-    Node *n3 = NULL;
-
-    n1 = new Node(10);
-    n2 = new Node(20);
-    n3 = new Node(30);
+    // Initialize nodes and create a linked list
+    Node *n1 = new Node(10);
+    Node *n2 = new Node(20);
+    Node *n3 = new Node(30);
     n1->next = n2;
     n2->next = n3;
-    head = n1;
+    head = n1; // Set the global head to the first node
 
     cout << "The current linked list: " << endl;
-    display(n1);
+    display(head);
 
+    // Add at the head
     addAtHead(&head, 4);
-    cout << "The new linked list: " << endl;
-    display(n1);
+    cout << "The new linked list after adding at head: " << endl;
+    display(head);
 
-    addAtTail(head, 40);
-    cout << "The new linked list: " << endl;
-    display(n1);
+    // Add at the tail
+    head = addAtTail(head, 40);
+    cout << "The new linked list after adding at tail: " << endl;
+    display(head);
 
-    cout << endl;
-    cout<<"The lenght of the linked list is: " << length(head) << endl;
+    // Display the length of the list
+    cout << "The length of the linked list is: " << length(head) << endl;
+
     return 0;
 }
