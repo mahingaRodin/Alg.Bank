b0c4e48dbcd831703ed63ab6194fe8451d70456b
P-Odiix <mahingarodin@gmail.com>
2025-01-15 09:41:28 +0200

    "updated "

--- LinkedLists/revision.cpp
+++ LinkedLists/revision.cpp
@@ -13,7 +13,7 @@ struct Node {
 
 void display(Node* head) {
     while(head != NULL) {
-        cout<<head->data<<" ";
+        cout<<head->data<<"->";
         head = head->next;
     }
     cout<<"NULL"<< endl;
@@ -24,8 +24,10 @@ int main() {
     Node* n1 = new Node(10);
     Node* n2 = new Node(20);
     Node *n3 = new Node(30);
+    Node *n4 = new Node(40);
     n1->next = n2;
     n2->next = n3;
-    display(n1);
+    n3->next = n4;
+    display(n3);
     return 0;
 }
