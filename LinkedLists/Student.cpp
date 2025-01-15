5d30b2be62d3bee40840119dc22cf98da4d1f3d2
P-Odiix <mahingarodin@gmail.com>
2025-01-15 10:03:46 +0200

    "implemented the student .cpp"

--- LinkedLists/Student.cpp
+++ LinkedLists/Student.cpp
@@ -8,6 +8,7 @@ struct Student {
     string level;
     int age;
     Student *next;
+    Student(){}
     Student(int c, string n, string sc, string l, int a) {
         code = c;
         name = n;
@@ -25,4 +26,20 @@ void display(Student *head) {
     cout << "NULL" << endl;
 }
 
+int main() {
+    Student *s1 = new Student();
+    s1->code = 101;
+    s1->name = "Rodin";
+    s1->school = "RCA";
+    s1->age = 17;
 
+    Student *s2 = new Student(102,"Peter", "RCA", "10", 17);
+    Student *s3 = new Student(102,"John", "RCA", "10", 17);
+    Student *s4 = new Student(102,"Mike", "RCA", "10", 19);
+    s1->next = s2;
+    s2->next = s3;
+    s3->next = s4;
+    s4->next = NULL;
+    display(s1);
+    return 0;
+}
