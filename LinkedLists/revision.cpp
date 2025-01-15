94e5e214e771046273f45e28348d05d0b1598aae
P-Odiix <mahingarodin@gmail.com>
2025-01-15 11:12:01 +0200

    "updated"

--- LinkedLists/revision.cpp
+++ LinkedLists/revision.cpp
@@ -3,18 +3,40 @@ using namespace std;
 
 
 //making it generic
-template<typename T>
 struct Node {
-    T data;
+    int data;
     Node *next;
-    Node(T d) {
+    Node(int d) {
         this->data = d;
         this->next = NULL;
     }
 };
 
+//adding at the beginning
+Node *addAtHead(Node *head, int data) {
+    Node *newNode = new Node(data);
+    newNode->next = head;
+    head = newNode;
+    return head;
+}
 
-void display(Node<double>  *head) {
+//add at tail
+Node *addAtTail(Node *head, int data) {
+    if(head == NULL) {
+        Node *newNode = new Node(data);
+        head = newNode;
+        return head;
+    }
+
+    while(head->next != NULL) {
+        head = head->next;
+    }
+
+    Node *newNode = new Node(data);
+    head->next = newNode;
+    return head;
+}
+void display(Node *head) {
     while(head != NULL) {
         cout<<head->data<<"->";
         head = head->next;
@@ -24,15 +46,20 @@ void display(Node<double>  *head) {
 
 
 int main() {
-    Node<double> *n1 = new Node<double>(10);
-    Node<double> *n2 = new Node<double> (20);
-    Node<double>  *n3 = new Node<double> (30);
-    Node<double>  *n4 = new Node<double> (40);
-    Node<double>  n5 (50.56);
+    Node *n1 = new Node(10);
+    Node *n2 = new Node(20);
+    Node *n3 = new Node(30);
+    Node *n4 = new Node(40);
     n1->next = n2;
     n2->next = n3;
     n3->next = n4;
-    n4->next = &n5;
+    n4->next = NULL;
     display(n1);
+
+    Node *head = addAtHead(n1, 100);
+    display(head);
+
+     addAtTail(head, 150);
+    display(head);
     return 0;
 }
