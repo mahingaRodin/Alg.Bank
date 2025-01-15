b882e768de5133044484d5db893b3203800c9a6f
P-Odiix <mahingarodin@gmail.com>
2025-01-15 09:49:19 +0200

    "made the program to be generic "

--- LinkedLists/revision.cpp
+++ LinkedLists/revision.cpp
@@ -1,17 +1,20 @@
 #include<iostream>
 using namespace std;
 
+
+//making it generic
+template<typename T>
 struct Node {
-    int data;
+    T data;
     Node *next;
-    Node(int data) {
-        this->data = data;
+    Node(T d) {
+        this->data = d;
         this->next = NULL;
     }
 };
 
 
-void display(Node* head) {
+void display(Node<double>  *head) {
     while(head != NULL) {
         cout<<head->data<<"->";
         head = head->next;
@@ -21,11 +24,11 @@ void display(Node* head) {
 
 
 int main() {
-    Node* n1 = new Node(10);
-    Node* n2 = new Node(20);
-    Node *n3 = new Node(30);
-    Node *n4 = new Node(40);
-    Node n5 (50);
+    Node<double> *n1 = new Node<double>(10);
+    Node<double> *n2 = new Node<double> (20);
+    Node<double>  *n3 = new Node<double> (30);
+    Node<double>  *n4 = new Node<double> (40);
+    Node<double>  n5 (50.56);
     n1->next = n2;
     n2->next = n3;
     n3->next = n4;
