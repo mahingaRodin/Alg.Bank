066bbb2fb750c86b77776f29c29dcd35b75c600f
P-Odiix <mahingarodin@gmail.com>
2025-01-15 08:13:55 +0200

    "implemented the addFirst and display methods in the circular linked list "

---
+++ LinkedLists/CircularLinkedList.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+
+struct Node {
+    int data;
+    Node* next;
+    Node(int data ) {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+void addFirst(Node*& tail , int data) {
+    Node *newNode = new Node(data);
+    if(tail == NULL) {
+        newNode->next = newNode;
+        tail = newNode;
+    } else {
+        newNode->next = tail->next;
+        tail->next = newNode;
+    }
+}
+
+void display(Node* tail) {
+    if(tail == NULL) {
+        cout << "list is empty" << endl;
+        return;
+    }
+    Node* temp = tail->next;
+    do
+        (
+            cout << temp->data << " ";
+            temp = temp->next;
+            ) while(temp != tail->next);
+    cout << endl;
+}
+
+
+int main() {
+
+}
