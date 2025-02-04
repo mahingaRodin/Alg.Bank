65a266f1d0ffe4e741305d334ebd00c49bcf3dd3
P-Odiix <mahingarodin@gmail.com>
2025-02-04 21:07:58 +0200

    "updated and compiled"

--- Queues/Priority_queue.cpp
+++ Queues/Priority_queue.cpp
@@ -1,59 +1,54 @@
-#include<iostream>
-using namespace std;
+#include <iostream>
 
-//implementation of the priority queue
+#define MAX_SIZE 100  // Maximum size of priority queue
 
-#define MAX_SIZE 100
+class PriorityQueue {
+private:
+    int heap[MAX_SIZE];  // Array to store heap elements
+    int size;  // Current size of the heap
 
-
-class PriorityQueue{
-    private:
-    int heap[MAX_SIZE];
-    int size;
-
-    void swap(int &a , int &b) {
+    // Helper function to swap two elements
+    void swap(int &a, int &b) {
         int temp = a;
         a = b;
         b = temp;
     }
 
-    //heapifyup(to maintain the property after inserttion)
-    void heapifyup(int index) {
+    // Heapify up (maintains heap property after insertion)
+    void heapifyUp(int index) {
         int parent = (index - 1) / 2;
-        while(index > 0 && heap[index] > heap[parent]) {
+        while (index > 0 && heap[index] > heap[parent]) {
             swap(heap[index], heap[parent]);
             index = parent;
             parent = (index - 1) / 2;
         }
     }
 
-    void heapifydown(int index) {
+    // Heapify down (maintains heap property after deletion)
+    void heapifyDown(int index) {
         int left = 2 * index + 1;
         int right = 2 * index + 2;
         int largest = index;
 
-        if(left < size && heap[left] > heap[largest]) {
+        if (left < size && heap[left] > heap[largest]) {
             largest = left;
         }
-
-        if(right < size && heap[right] > heap[largest]) {
+        if (right < size && heap[right] > heap[largest]) {
             largest = right;
         }
-
-        if(largest != index) {
+        if (largest != index) {
             swap(heap[index], heap[largest]);
-            heapifydown(largest);
+            heapifyDown(largest);
         }
     }
 
-    public:
-    //constuctor 
+public:
+    // Constructor
     PriorityQueue() {
         size = 0;
     }
-    
 
-    //insert an element into the priority queue
+    // Insert an element into the priority queue
     void push(int value) {
         if (size == MAX_SIZE) {
             std::cout << "Priority Queue is full!\n";
@@ -64,38 +59,40 @@ class PriorityQueue{
         size++;
     }
 
-       // Remove and return the highest priority element
+    // Remove and return the highest priority element
     int pop() {
         if (size == 0) {
             std::cout << "Priority Queue is empty!\n";
             return -1;
         }
-        int maxVal = heap[0];
+        int maxValue = heap[0];
         heap[0] = heap[size - 1];
         size--;
         heapifyDown(0);
-        return maxVal;
+        return maxValue;
     }
 
-    //get the highest priority 
+    // Get the highest priority element without removing it
     int top() {
-        if(size == 0) {
-            std::cout<<"priority queue is empty";
+        if (size == 0) {
+            std::cout << "Priority Queue is empty!\n";
             return -1;
         }
         return heap[0];
     }
 
+    // Check if the queue is empty
     bool empty() {
         return size == 0;
     }
 
-    //get the size of the priority queue
+    // Get the size of the priority queue
     int getSize() {
         return size;
     }
 };
 
+// Main function to test the priority queue
 int main() {
     PriorityQueue pq;
 
@@ -105,9 +102,9 @@ int main() {
     pq.push(50);
     pq.push(20);
 
-    std::cout << "Top element: " << pq.top() << std::endl;
-    
-    while(!pq.empty()) {
+    std::cout << "Top element: " << pq.top() << std::endl;  // Output: 50
+
+    while (!pq.empty()) {
         std::cout << "Removing: " << pq.pop() << std::endl;
     }
 
