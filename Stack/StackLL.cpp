2cf81cbd4b75417ed62006bdc9e1e58b3ee564b1
P-Odiix <mahingarodin@gmail.com>
2025-02-12 10:24:01 +0200

    "added the stack class "

---
+++ Stack/StackLL.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+
+//implementing stack using linked list 
+
+//initialize the linked list node 
+struct Node {
+    int data;
+    Node* next;
+    Node(int data) {
+        this->data = data;
+        next = NULL;
+    }
+}
+
+// initialize the stack class
+class Stack {
+    private:
+        Node* top; // the top of the stack
+
+        public:
+            Stack() { top = NULL; }
+
+
+            void push(int value) {
+                Node *newNode = new Node(value);
+                newNode->next = top;
+                top = newNode;
+                cout << value << " pushed to stack\n";
+            } 
+
+            void pop() {
+                if(!top) {
+                    cout<<"stack underflow!\n";
+                    return;
+                }
+                cout << top->data << " popped from stack\n";
+                Node* temp = top;
+                top = top->next;
+                delete temp;
+            }
+
+            int peek() {
+                if(!top) {
+                    cout << "Stack is empty!\n";
+                    return -1;
+                }
+                return top->data;
+            }
+
+            bool isEmpty() {
+                return top = NULL;
+            }
+};
+
